f853420d281bdaa840a42bfb9592afa2c6a28a14
aatarasoff <aatarasoff@gmail.com>
2022-09-09 15:32:15 +0400

    upgrade LINKERD_VERSION build arg

--- build-injector.sh
+++ build-injector.sh
@@ -2,4 +2,4 @@
 
 set -eu
 
-docker build --build-arg LINKERD_VERSION=2.11.4 -f ./injector/Dockerfile -t aatarasoff/easyauth-webhook:${TAG:-latest} .
+docker build --build-arg LINKERD_VERSION=2.12.0 -f ./injector/Dockerfile -t aatarasoff/easyauth-webhook:${TAG:-latest} .
